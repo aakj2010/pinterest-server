3aab6fb8fd49f9152b2f6d5a385df5d758c86766
aakj2010 <aakj2010@gmail.com>
2022-11-07 07:19:50 +0530

    netlify

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require('colors')
+const cors = require("cors")
 const dotenv = require("dotenv").config();
 const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -9,6 +10,15 @@ const port = process.env.PORT || 5000
 connectDB()
 
 const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin: "*",
+    credentials: true
+
+}))
+
+
 
 app.use(express.json())
 
