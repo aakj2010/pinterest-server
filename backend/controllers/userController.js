5cbd9e38a0a649f50968fa6f21fa38e4a5425cbf
aakj2010 <aakj2010@gmail.com>
2022-11-07 18:07:09 +0530

    latest

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,7 +88,7 @@ const getMe = asyncHandler(async (req, res) => {
 
 // Generate JWT Token
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' })
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '3h' })
 }
 
 
--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+
 app.use(cors({
     origin: "*",
     credentials: true
