5d44aa354e598fd5ca25c36aa1ea3e8ba7b79e73
aakj2010 <aakj2010@gmail.com>
2023-01-06 19:09:07 +0530

    dep

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
 
+app.get("/", (req, res) =>
+  res.send(`Server Running`)
+);
 
 app.use('/api/users', require('./routes/userRoutes'))
 
