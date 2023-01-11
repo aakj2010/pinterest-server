5991a0b4c4f0c836ffa90f467da3c3d24d26d48c
aakj2010 <aakj2010@gmail.com>
2023-01-11 20:15:50 +0530

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,53 @@
-const mongoose = require('mongoose')
-const dotenv = require("dotenv").config()
-mongoose.set("strictQuery", true)
+// const mongoose = require('mongoose')
+// const dotenv = require("dotenv").config()
+// // mongoose.set("strictQuery", true)
 
-const connectDB = async ()=>{
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+// const connectDB = async ()=>{
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGO_URI)
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-    } catch (error) {
-        console.log(error);
-        process.exit(1)
-    }
+//         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1)
+//     }
+// }
+
+// module.exports = connectDB
+
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect(process.env.MONGO_URI);
+
+// const connection = mongoose.connection;
+
+// connection.on("connected", () => {
+//   console.log("Mongo DB Connection Successfull");
+// });
+
+// connection.on("error", () => {
+//   console.log("Mongo DB Connection failed");
+// });
+
+// module.exports = mongoose;
+
+
+
+
+const mongoose = require("mongoose");
+require('dotenv').config();
+dbConnect();
+async function dbConnect() {
+  try {
+    await mongoose.connect(
+        `mongodb+srv://Arslan:admin123@cluster0.jiog1sg.mongodb.net/pinterest?retryWrites=true&w=majority`,
+      { useNewUrlParser: true }
+    );
+    console.log("MongoDb connected successfully");
+  } catch (error) {
+    console.log("MongoDb connection failed");
+  }
 }
 
-module.exports = connectDB
+module.exports = mongoose;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
+const app = express()
 const colors = require('colors')
 const cors = require("cors")
-const dotenv = require("dotenv").config();
-const connectDB = require('./config/db')
+require("dotenv").config();
+// const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
-
-connectDB()
-
-const app = express()
+const connectDB = require('./config/db')
 
 app.use(express.json())
+// connectDB();
+
+
 
 // app.use(cors({ origin: "https://pinterest-delta.vercel.app/" }))
 
@@ -20,9 +21,9 @@ app.use(cors({
   credentials: true
 }))
 
-app.use(express.json())
 
-app.use(express.urlencoded({ extended: false }))
+
+// app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) =>
   res.send(`Server Running`)
